c238b154736a9d6158da7c400275921f98b11f1b
hemant gavali <digitaluser197@gmail.com>
2025-09-26 23:59:26 +0530

    hello

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,10 +1,20 @@
 import React, { useState } from 'react'
 
 const Footer = () => {
-  function
+  function UserList(){
+    const users = ["ram", "bam", "kam"];
+    return(
+      <ul>
+        {users.map((user, index)=>(
+          <li key={index}>{user}</li>
+        ))}
+      </ul>
+    )
+  }
 
   return (
     <div>
+      <UserList />
         <Name year = "1974" company = "old monk" />
         <button>Cheack login Status here</button>
     </div>
