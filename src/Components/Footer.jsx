b435e2aeff1e2f438e4edf5fc916c45ac04a49dd
hemant gavali <digitaluser197@gmail.com>
2025-09-26 23:28:02 +0530

    use state

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,9 +4,19 @@ const Footer = () => {
   return (
     <div>
         <Name year = "1974" company = "old monk" />
+        <button onClick={()=>loginStatus}>Cheack login Status here</button>
     </div>
   )
 }
+
+function loginStatus({isLoged}) {
+  if(isLoged){
+    return <h1>Login Here</h1>
+  }else{
+    return <h1>Not login here</h1>
+  }
+}
+const isLoged = true;
 
 function Name({year, company}) {
     return(
