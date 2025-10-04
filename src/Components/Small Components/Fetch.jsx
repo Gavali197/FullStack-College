1d372d2d83b1da6fe94205081677bd7f8344678b
hemant gavali <digitaluser197@gmail.com>
2025-10-05 01:02:50 +0530

    fetching data using async await

--- src/Components/Small Components/Fetch.jsx
+++ src/Components/Small Components/Fetch.jsx
@@ -1,27 +1,72 @@
+// import React, { useEffect, useState } from 'react'
+
+// function Fetch() {
+//     const [user, setuser] = useState([]);
+
+//     useEffect(()=>{
+//         fetch("https://jsonplaceholder.typicode.com/users")
+//         .then((responce)=>responce.json())
+//         .then((data)=> setuser(data))
+//         .catch((error)=> console.log("Error of fetching data : ", error));
+//     }, []);
+
+//   return (
+//     <div>
+//         <ul>
+//              {user.map((users)=>(
+//                 <li key={users.id}>
+//                     {users.name} -- {users.email}
+//                 </li>
+//              ))}
+//         </ul>
+//     </div>
+//   )
+// }
+
+// export default Fetch
+
+
+
+
 import React, { useEffect, useState } from 'react'
 
 function Fetch() {
-    const [user, setuser] = useState([]);
+  const [product, setproduct] = useState([]);
+  const [loading, setloding] = useState(true);
 
-    useEffect(()=>{
-        fetch("https://jsonplaceholder.typicode.com/users")
-        .then((responce)=>responce.json())
-        .then((data)=> setuser(data))
-        .catch((error)=> console.log("Error of fetching data : ", error));
-    }, []);
+  useEffect(()=>{
+    const fetchApi = async () => {
+        try {
+            const res = await fetch("https://fakestoreapi.com/products");
+            const data = await res.json();
+            setproduct(data);
+        } catch (error) {
+            console.error("Errro => :", error);
+        } finally{
+            setloding(false);
+        }
+    }
 
-  return (
+    fetchApi();
+  },[]);
+
+
+  if(loading) return <p>Loading...Page...</p>
+
+
+    return (
     <div>
+        <h2>Products</h2>
         <ul>
-             {user.map((users)=>(
-                <li key={users.id}>
-                    {users.name} -- {users.email}
-                </li>
-             ))}
+            {product.map((item)=>(
+                <div key={item.id}>
+                    <h3>{item.title}</h3>
+                    <p>Prices : {item.price}</p>
+                </div>
+            ))}
         </ul>
     </div>
   )
 }
 
 export default Fetch
-
