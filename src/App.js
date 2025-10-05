fcd662183b62c655ed928c2e06315eafc9a72eba
hemant gavali <digitaluser197@gmail.com>
2025-10-06 01:17:58 +0530

    product as well

--- src/App.js
+++ src/App.js
@@ -1,61 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
-import Name from './Components/Name';
-import Header from './Components/Header';
-import Footer from './Components/Footer';
-import Card from './Components/Card';
-import Counter from './Components/Counter';
-import CounterApp from './Components/Small Components/CounterApp';
-import LoginForm from './Components/Small Components/LoginForm';
-import Products from './Components/Small Components/Products';
-import TodosApp from './Components/Small Components/TodosApp';
-import UserList from './Components/Small Components/UserList';
-import StylePractice from './Components/Small Components/StylePractice';
-import ControlledForm from './Components/Small Components/ControlledForm';
-import UncontrolledForm from './Components/Small Components/UncontrolledForm';
-import LoginForm1 from './Mini Project/LoginForm1';
-import UseEffects from './Components/UseEffects';
-import Clock from './Components/Small Components/Clock';
-import Fetch from './Components/Small Components/Fetch';
-import Fetch_PostList from './Components/Small Components/Fetch_PostList';
-
-
-
+import logo from "./logo.svg";
+import "./App.css";
+import Name from "./Components/Name";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Card from "./Components/Card";
+import Counter from "./Components/Counter";
+import CounterApp from "./Components/Small Components/CounterApp";
+import LoginForm from "./Components/Small Components/LoginForm";
+import Products from "./Components/Small Components/Products";
+import TodosApp from "./Components/Small Components/TodosApp";
+import UserList from "./Components/Small Components/UserList";
+import StylePractice from "./Components/Small Components/StylePractice";
+import ControlledForm from "./Components/Small Components/ControlledForm";
+import UncontrolledForm from "./Components/Small Components/UncontrolledForm";
+import LoginForm1 from "./Mini Project/LoginForm1";
+import UseEffects from "./Components/UseEffects";
+import Clock from "./Components/Small Components/Clock";
+import Fetch from "./Components/Small Components/Fetch";
+import Fetch_PostList from "./Components/Small Components/Fetch_PostList";
 
 function App() {
   return (
-
-
     <div className="App">
-
       {/* <StylePractice />   */}
 
-      <div className='Counter'>
-        {/* <CounterApp /> */}
-      </div>
+      <div className="Counter">{/* <CounterApp /> */}</div>
 
       {/* <Products /> */}
 
-
       {/* <TodosApp /> */}
 
+      <div className="userlist">{/* <UserList /> */}</div>
 
+      <div className="login">{/* <LoginForm /> */}</div>
 
-      <div className='userlist'>
-        {/* <UserList /> */}
-      </div>
-
-
-
-      <div className='login'>
-        {/* <LoginForm /> */}
-      </div>
-
-
-
-      <div className="header">
-        {/* <Header />   */}
-      </div>
+      <div className="header">{/* <Header />   */}</div>
       {/* <StylePractice /> */}
       <Clock />
 
@@ -84,8 +63,6 @@ function App() {
       {/* <LoginForm1 /> */}
     </div>
   );
-
-
 }
 
 export default App;
