e4e3fae8fcfbb682de1db607f070a0b4c66b7892
hemant gavali <digitaluser197@gmail.com>
2025-10-08 21:41:33 +0530

    unctrolled form

--- src/Components/Small Components/UncontrolledForm.jsx
+++ src/Components/Small Components/UncontrolledForm.jsx
@@ -1,28 +1,46 @@
+// import React, { useRef } from 'react'
+
+// function UncontrolledForm() {
+//     const nameRef = useRef();
+    
+//     const handleSubmit = (e) => {
+//         e.preventDefault();
+//         alert(`Hello ${nameRef.current.value}`);
+//         nameRef.current.value = "";
+//     };
+
+
+//     return (
+//     <div>
+//         <form onSubmit={handleSubmit}>
+//             <label>
+//             name : 
+//             <input type="text" ref={nameRef} />    
+//             </label>    
+//             <button type='submit'>Submit</button>
+//         </form>      
+//     </div>
+//   )
+
+
+// }
+
+// export default UncontrolledForm
+
+
 import React, { useRef } from 'react'
 
-function UncontrolledForm() {
-    const nameRef = useRef();
-    
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        alert(`Hello ${nameRef.current.value}`);
-        nameRef.current.value = "";
-    };
 
+
+function UncontrolledForm() {
+
+    const inputFocus = useRef(null);
 
     return (
     <div>
-        <form onSubmit={handleSubmit}>
-            <label>
-            name : 
-            <input type="text" ref={nameRef} />    
-            </label>    
-            <button type='submit'>Submit</button>
-        </form>      
+
     </div>
   )
-
-
 }
 
 export default UncontrolledForm
