6c51cdec560442469502d3764e1c879cf23c61d8
hemant gavali <digitaluser197@gmail.com>
2025-10-08 22:02:41 +0530

    uncontrolled form

--- src/Components/Small Components/UncontrolledForm.jsx
+++ src/Components/Small Components/UncontrolledForm.jsx
@@ -2,52 +2,70 @@
 
 // function UncontrolledForm() {
 //     const nameRef = useRef();
-    
+
 //     const handleSubmit = (e) => {
 //         e.preventDefault();
 //         alert(`Hello ${nameRef.current.value}`);
 //         nameRef.current.value = "";
 //     };
 
-
 //     return (
 //     <div>
 //         <form onSubmit={handleSubmit}>
 //             <label>
-//             name : 
-//             <input type="text" ref={nameRef} />    
-//             </label>    
+//             name :
+//             <input type="text" ref={nameRef} />
+//             </label>
 //             <button type='submit'>Submit</button>
-//         </form>      
+//         </form>
 //     </div>
 //   )
-
 
 // }
 
 // export default UncontrolledForm
 
+// import React, { useRef } from 'react'
 
-import React, { useRef } from 'react'
+// function UncontrolledForm() {
+//    const inputRef = useRef(null);
 
+//   const handleFocus = () => {
+//     // Step 2: Access input DOM element directly
+//     inputRef.current.focus();
+//   };
 
+//   return (
+//     <div>
+//       <h2>useRef Example 🚀</h2>
+//       <input ref={inputRef} type="text" placeholder="Click the button below..." />
+//       <br />
+//       <button onClick={handleFocus}>Focus Input</button>
+//     </div>
+//   );
+// }
+
+// export default UncontrolledForm
+
+// ex 2
+
+import React, { useEffect, useRef, useState } from "react";
 
 function UncontrolledForm() {
-   const inputRef = useRef(null);
+  const [count, setcount] = useState(0);
+  const prevCount = useRef(0);
 
-  const handleFocus = () => {
-    // Step 2: Access input DOM element directly
-    inputRef.current.focus();
-  };
+  useEffect(() => {
+    prevCount.current = count;
+  }, [count]);
 
   return (
     <div>
-      <h2>useRef Example 🚀</h2>
-      <input ref={inputRef} type="text" placeholder="Click the button below..." />
-      <br />
-      <button onClick={handleFocus}>Focus Input</button>
+      <h2>Current: {count}</h2>
+      <h3>Previous: {prevCount.current}</h3>
+      <button onClick={() => setcount(count + 1)}>Increase</button>
     </div>
   );
 }
 
-export default UncontrolledForm
+export default UncontrolledForm;
