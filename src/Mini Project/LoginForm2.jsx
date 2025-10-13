ba491cfdf3c7617f77b065da75df6a015b8bb898
hemant gavali <digitaluser197@gmail.com>
2025-10-14 02:36:24 +0530

    LoginForm 2

--- src/Mini Project/LoginForm2.jsx
+++ src/Mini Project/LoginForm2.jsx
@@ -2,31 +2,38 @@ import React, { useState } from "react";
 import "./Text.css";
 
 function LoginForm2() {
-    const [form, setform] = useState({ email: "", password: "" }); 
-    const [error, seterror] = useState("");
+  const [form, setform] = useState({ email: "", password: "" });
+  const [error, seterror] = useState("");
 
-    const handlesubmit = (e) =>{
-        e.prevantDefault();
+  const handlesubmit = (e) => {
+    e.prevantDefault();
 
-        if(!form.email || !form.password){
-            seterror("All Field are required so fill all input filed");
-        }else if(!form.email.includes("@")){
-            seterror("Enter Valid email");
-        }else if(!form.password < 6){
-            seterror("Password Should Be at least 6");
-        }else{
-            alert("Form Submit Successfully...");
-        }
+    if (!form.email || !form.password) {
+      seterror("All Field are required so fill all input filed");
+    } else if (!form.email.includes("@")) {
+      seterror("Enter Valid email");
+    } else if (!form.password < 6) {
+      seterror("Password Should Be at least 6");
+    } else {
+      alert("Form Submit Successfully...");
     }
+  };
 
-    return (
+  const onChange = (e) => {
+    const { email, value } = e.target;
+    setform({ ...form, [email]: value });
+  };
+
+  return (
     <div>
       <div className="container">
-        <div className="header"><h2>Login Form 2</h2></div>
-        <form>
+        <div className="header">
+          <h2>Login Form 2</h2>
+        </div>
+        <form onSubmit={handlesubmit}>
           <div className="email-grp">
             <label>Email</label>
-            <input type="text" />
+            <input type="text" value={form.email} />
           </div>
           <div className="password-grp">
             <label>Password</label>
