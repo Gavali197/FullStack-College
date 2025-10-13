9a78737c439af7244bf2bd086d56ba7bcbd06803
hemant gavali <digitaluser197@gmail.com>
2025-10-14 02:46:02 +0530

    LoginForm 2

--- src/Mini Project/LoginForm2.jsx
+++ src/Mini Project/LoginForm2.jsx
@@ -6,22 +6,23 @@ function LoginForm2() {
   const [error, seterror] = useState("");
 
   const handlesubmit = (e) => {
-    e.prevantDefault();
+    e.preventDefault();
 
     if (!form.email || !form.password) {
       seterror("All Field are required so fill all input filed");
     } else if (!form.email.includes("@")) {
       seterror("Enter Valid email");
-    } else if (!form.password < 6) {
+    } else if (form.password.length < 6) {
       seterror("Password Should Be at least 6");
     } else {
+        seterror("");
       alert("Form Submit Successfully...");
     }
   };
 
   const onChange = (e) => {
-    const { email, value } = e.target;
-    setform({ ...form, [email]: value });
+    const { name, value } = e.target;
+    setform({ ...form, [name]: value });
   };
 
   return (
@@ -33,15 +34,16 @@ function LoginForm2() {
         <form onSubmit={handlesubmit}>
           <div className="email-grp">
             <label>Email</label>
-            <input type="text" value={form.email} />
+            <input type="text" name="email" value={form.email} onChange={onChange}/>
           </div>
           <div className="password-grp">
             <label>Password</label>
-            <input type="text" />
+            <input type="password" name="password" value={form.password} onChange={onChange} />
           </div>
           <div className="btn">
             <button id="btn">Login here</button>
           </div>
+           {error && <p style={{ color: "red" }}>{error}</p>}
         </form>
       </div>
     </div>
