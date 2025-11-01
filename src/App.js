7a2722ac5e4e913111d8cf19366e7d684ef3b4bc
hemant gavali <digitaluser197@gmail.com>
2025-11-02 02:24:22 +0530

    app.js file was dead

--- src/App.js
+++ src/App.js
@@ -1,36 +1,34 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./App.css";
+// import React from "react";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import "./App.css";
 
-// Contexts
-import ThemeContext from "./Components/Concept/ContexsApi/ThemeContext";
-import Usercontext from "./Components/Concept/ContexsApi/Usercontext";
+// // Contexts
+// import ThemeContext from "./Components/Concept/ContexsApi/ThemeContext";
+// import Usercontext from "./Components/Concept/ContexsApi/Usercontext";
 
-// Components
-import Header from "./Components/Header";
-import Footer from "./Components/Footer";
-import Profile from "./Components/Concept/ContexsApi/Profile";
-import UserFormContext from "./Components/Concept/UserFormContext";
-import RegistrationForm from "./Mini Project/RegistrationForm";
-import LoginForm2 from "./Mini Project/LoginForm2";
-import FancyLogin from "./Mini Project/FancyLogin1";
-import Home from "./Exam/Home";
-import FormWithSession from "./Exam/Form";
+// // Components
+// import Header from "./Components/Header";
+// import Footer from "./Components/Footer";
+// import Profile from "./Components/Concept/ContexsApi/Profile";
+// import UserFormContext from "./Components/Concept/UserFormContext";
+// import RegistrationForm from "./Mini Project/RegistrationForm";
+// import LoginForm2 from "./Mini Project/LoginForm2";
+// import FancyLogin from "./Mini Project/FancyLogin1";
+// import Home from "./Exam/Home";
+// import FormWithSession from "./Exam/Form";
 
-function App() {
-  const [theme, setTheme] = React.useState("light");
+// function App() {
+//   const [theme, setTheme] = React.useState("light");
 
-  const toggleTheme = () => {
-    setTheme(theme === "light" ? "dark" : "light");
-  };
+//   const toggleTheme = () => {
+//     setTheme(theme === "light" ? "dark" : "light");
+//   };
 
-  return (
-    <div className="app">
+//   return (
+//     <div className="app">
                   
-    </div>
-  );
-}
+//     </div>
+//   );
+// }
 
-export default App;
-
-
+// export default App;
