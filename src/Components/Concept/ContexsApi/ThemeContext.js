47ca82f42e19f76ce4254bb55afe568abe7aa52a
hemant gavali <digitaluser197@gmail.com>
2025-11-09 23:31:03 +0530

    theme context

--- src/Components/Concept/ContexsApi/ThemeContext.js
+++ src/Components/Concept/ContexsApi/ThemeContext.js
@@ -1,5 +1,38 @@
+const { createContext, useReducer, useContext } = require("react");
+
 const theame = {
-  light : {backgraund : '#ffffff', color: '#000000' },
-  dark : {backgraund : '#000000', color:'#ffffff'}
+  light: { backgraund: "#ffffff", color: "#000000" },
+  dark: { backgraund: "#000000", color: "#ffffff" },
 };
 
+const theameReducer = (state, action) => {
+  switch (action.type) {
+    case "TOGGLE_THEAME":
+      return state.mode === "light"
+        ? { mode: "dark", ...theame.dark }
+        : { mode: "light", ...theame.light };
+    default:
+      return state;
+  }
+};
+
+const initialState = { mode: "light", ...theame.light };
+
+const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(theameReducer, initialState);
+  return (
+    <ThemeContext.Provider
+      value={{
+        theame: state,
+        toggleTheme: () => dispatch({ type: "TOGGLE_THEME" }),
+      }}
+    >
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+
+export const useTheme = useContext(ThemeContext);
