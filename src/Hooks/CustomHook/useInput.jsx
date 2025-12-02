de56017253eff1986a201c91bf516f4e243da8a7
hemant gavali <digitaluser197@gmail.com>
2025-12-02 22:58:36 +0530

    useinput

---
+++ src/Hooks/CustomHook/useInput.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+
+function useInput(initialValue = "") {
+    const [value, setvalue] = useState(initialValue);
+
+    const onChange = (e) => {
+        setvalue(e.target.value);
+    }
+
+    const reset = () => setvalue("");
+
+  return {value, onChange, reset}
+}
+
+export default useInput
