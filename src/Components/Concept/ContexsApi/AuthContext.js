2e35d80e89f48322e2c0ed6ba6442cb0429856e2
hemant gavali <digitaluser197@gmail.com>
2025-12-06 23:50:51 +0530

    build own authentication context

---
+++ src/Components/Concept/ContexsApi/AuthContext.js
@@ -0,0 +1,19 @@
+import React, { createContext, useEffect, useState } from 'react'
+
+export const AuthContext = createContext();
+
+export function AuthProvide( {children} ) {
+    const [user, setuser] = useState(JSON.parse(localStorage.getItem("user")) || null);
+
+    useEffect(()=>{
+        if(user) localStorage.setItem("user", JSON.stringify(user));
+        else localStorage.removeItem("user");
+    }, [user]);
+
+    const login = (email, password) => {
+        if(email === "admin123@gmail.com" || password === "1234"){
+            setuser({email, role:"admin"});
+            return {success:true};
+        }
+    }
+}
