ac6556f52fa654410446916394eb2be02ae0ab36
hemant gavali <digitaluser197@gmail.com>
2025-12-06 23:54:49 +0530

    set up context api

--- src/Components/Concept/ContexsApi/AuthContext.js
+++ src/Components/Concept/ContexsApi/AuthContext.js
@@ -1,19 +1,31 @@
-import React, { createContext, useEffect, useState } from 'react'
+import React, { createContext, useEffect, useState } from "react";
 
 export const AuthContext = createContext();
 
-export function AuthProvide( {children} ) {
-    const [user, setuser] = useState(JSON.parse(localStorage.getItem("user")) || null);
+export function AuthProvide({ children }) {
+  const [user, setuser] = useState(
+    JSON.parse(localStorage.getItem("user")) || null
+  );
 
-    useEffect(()=>{
-        if(user) localStorage.setItem("user", JSON.stringify(user));
-        else localStorage.removeItem("user");
-    }, [user]);
+  useEffect(() => {
+    if (user) localStorage.setItem("user", JSON.stringify(user));
+    else localStorage.removeItem("user");
+  }, [user]);
 
-    const login = (email, password) => {
-        if(email === "admin123@gmail.com" || password === "1234"){
-            setuser({email, role:"admin"});
-            return {success:true};
-        }
+  const login = (email, password) => {
+    if (email === "admin123@gmail.com" || password === "1234") {
+      setuser({ email, role: "admin" });
+      return { success: true };
+    } else if (email === "user123@gmail.com" || password === "1234") {
+      setuser({ email, role: "user" });
+      return { success: true };
     }
+    return { success: false, message: "invalide creadition" };
+  };
+
+  const logout = () => {
+    setuser(null);
+  };
+
+  return <AuthContext.Provider>{children}</AuthContext.Provider>;
 }
