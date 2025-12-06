99db1ce884c402a30956ec281b69e498bd8c83b7
hemant gavali <digitaluser197@gmail.com>
2025-12-07 00:05:07 +0530

    build protect route

---
+++ src/Auth/ProtectRoute.js
@@ -0,0 +1,14 @@
+import React, { useContext } from 'react'
+import { AuthContext } from '../Components/Concept/ContexsApi/AuthContext'
+import { Navigate } from 'react-router-dom';
+
+function ProtectRoute({children}) {
+    const {user} = useContext(AuthContext);
+
+    if(!user){
+        return <Navigate to="/login" replace />
+    }
+  return children;
+}
+
+export default ProtectRoute
