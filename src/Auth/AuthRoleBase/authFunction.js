030362be4834685121f6fbab6577e0cc6a392a8c
hemant gavali <digitaluser197@gmail.com>
2025-12-10 12:50:58 +0530

    open data

--- src/Auth/AuthRoleBase/authFunction.js
+++ src/Auth/AuthRoleBase/authFunction.js
@@ -1,15 +1,32 @@
 import { useNavigate } from "react-router-dom";
 
 export const registerUser = (userData) => {
-    const users = JSON.parse(localStorage.getItem("users")) || [];
+  const users = JSON.parse(localStorage.getItem("users")) || [];
 
-    const exits = users.find(u => u.email === userData.email);
+  const exits = users.find((u) => u.email === userData.email);
 
-    if(exits){
-        return {success:false, message: "Email Allready Register"};
-    }
+  if (exits) {
+    return { success: false, message: "Email Allready Register" };
+  }
 
-    users.push(userData);
-    localStorage.setItem("users", JSON.stringify(users));
-   
-} 
+  users.push(userData);
+  localStorage.setItem("users", JSON.stringify(users));
+};
+
+export const loginUser = (email, password, remember) => {
+  const users = JSON.parse(localStorage.getItem("users")) || [];
+
+  const user = users.find((u) => u.email === email && u.password === password);
+
+  if (!user) {
+    return { success: false, message: "Invalid Email or password" };
+    alert("User Are Not exit register user");
+  }
+
+  localStorage.setItem("currentUser", JSON.stringify(user));
+
+  if (remember) {
+    localStorage.setItem("rememberUser", JSON.stringify(user));
+  }
+  return { success: true, message: "login Successfully", user };
+};
