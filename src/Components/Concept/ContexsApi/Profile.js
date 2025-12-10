18c05726dde186e856c24a720faa9452629d7350
hemant gavali <digitaluser197@gmail.com>
2025-12-10 11:58:31 +0530

    Role based access

---
+++ src/Components/Concept/ContexsApi/Profile.js
@@ -0,0 +1,34 @@
+// import ProtectedRoute from "./components/ProtectedRoute";
+// import Cart from "./pages/Cart";
+// import Profile from "./pages/Profile";
+// import Orders from "./pages/Orders";
+
+// <Routes>
+//   <Route path="/login" element={<Login />} />
+
+//   {/* Protected pages */}
+//   <Route
+//     path="/cart"
+//     element={
+//       <ProtectedRoute>
+//         <Cart />
+//       </ProtectedRoute>
+//     }
+//   />
+//   <Route
+//     path="/profile"
+//     element={
+//       <ProtectedRoute>
+//         <Profile />
+//       </ProtectedRoute>
+//     }
+//   />
+//   <Route
+//     path="/orders"
+//     element={
+//       <ProtectedRoute>
+//         <Orders />
+//       </ProtectedRoute>
+//     }
+//   />
+// </Routes>
