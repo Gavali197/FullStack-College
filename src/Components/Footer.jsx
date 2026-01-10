0e1ecfc60853710b79feb7bb596f15f00994861e
hemant gavali <digitaluser197@gmail.com>
2026-01-10 23:28:39 +0530

    push some new

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+
+const Footer = () => {
+  function UserList(){
+    const users = ["ram", "bam", "kam"];
+    return(
+      <ul>
+        {users.map((user, index)=>(
+          <li key={index}>{user}</li>
+        ))}
+      </ul>
+    )
+  }
+
+  return (
+    <div>
+      <UserList />
+        <Name year = "1974" company = "old monk" />
+        <button>Cheack login Status here</button>
+    </div>
+  )
+}
+
+
+
+
+function Name({year, company}) {
+    return(
+        <div>
+        {year}
+        <br/>
+        {company}
+        </div>
+    )
+}
+
+export default Footer
