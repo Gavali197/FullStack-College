924d61ebebf3dad754e6685c7ff0dae7b6746aec
hemant gavali <digitaluser197@gmail.com>
2026-01-25 00:00:58 +0530

    passed json file

---
+++ Backend/Express/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+
+
+//passed json file in brwoser
+app.get("/user", (req, res)=>{
+    res.json({
+        name: "Hemant",
+        age: "21"
+    })
+    // res.send("Hello world my frist server are running");
+})
+
+app.listen(5000, ()=>{
+    console.log("Server Running 5000 port");
+})
+
