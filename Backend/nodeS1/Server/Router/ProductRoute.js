c6de5fcc46bc073d49a0308357a3ebcf7ec2a1d3
hemant gavali <digitaluser197@gmail.com>
2026-01-26 00:17:09 +0530

    Router Setup

--- Backend/nodeS1/Server/Router/ProductRoute.js
+++ Backend/nodeS1/Server/Router/ProductRoute.js
@@ -1,1 +1,15 @@
-console.log("ROuter")
+const express = require("express");
+const router = express.Router();
+
+const {
+    getProduct,
+    addProduct,
+    deleteProduct
+} = require("../controller/ProductConttroller");
+const { route } = require("../app");
+
+router.get("/", getProduct);
+router.post("/add", addProduct);
+router.delete("/delete", deleteProduct);
+
+module.exports = router;
