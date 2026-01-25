13213f5aad2f35fbf8ab5b7e0cd66967762b477e
hemant gavali <digitaluser197@gmail.com>
2026-01-25 23:52:23 +0530

    git add

--- Backend/nodeS1/Server/i1.js
+++ Backend/nodeS1/Server/i1.js
@@ -10,25 +10,25 @@ console.log("close");
 
 console.log("hello node js how are you");*/
 
-// const http = require("http");
-// const server = http.createServer((req, res)=> {
-//     res.write("Hello world how are you");
-//     res.end();
-// })
+const http = require("http");
+const server = http.createServer((req, res)=> {
+    res.write("Hello world how are you");
+    res.end();
+})
 
-// server.listen(5000, ()=>{
-//     console.log("Server running 5000 port");
-// }); 
+server.listen(5000, ()=>{
+    console.log("Server running 5000 port");
+}); 
 
-// const express = require("express");
-// const app = express();
+const express = require("express");
+const app = express();
 
-// // Define a route
-// app.get("/app", (req, res) => {
-//     res.send("Hello world post on there server");
-// });
+// Define a route
+app.get("/app", (req, res) => {
+    res.send("Hello world post on there server");
+});
 
-// // Start the server
-// app.listen(5000, () => {
-//     console.log("Server running on port 5000");
-// });
+// Start the server
+app.listen(5000, () => {
+    console.log("Server running on port 5000");
+});
