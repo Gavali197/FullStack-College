b6fc6888d1103a7503e765443603bae3ed1d6383
hemant gavali <digitaluser197@gmail.com>
2026-01-25 23:51:50 +0530

    start advance express

---
+++ Backend/nodeS1/Server/i1.js
@@ -0,0 +1,34 @@
+/*console.log("start");
+
+let Time  = setTimeout(() => {
+    console.log("running");
+}, 2000);
+
+if(Time._onTimeout){
+console.log("close");
+}
+
+console.log("hello node js how are you");*/
+
+// const http = require("http");
+// const server = http.createServer((req, res)=> {
+//     res.write("Hello world how are you");
+//     res.end();
+// })
+
+// server.listen(5000, ()=>{
+//     console.log("Server running 5000 port");
+// }); 
+
+// const express = require("express");
+// const app = express();
+
+// // Define a route
+// app.get("/app", (req, res) => {
+//     res.send("Hello world post on there server");
+// });
+
+// // Start the server
+// app.listen(5000, () => {
+//     console.log("Server running on port 5000");
+// });
