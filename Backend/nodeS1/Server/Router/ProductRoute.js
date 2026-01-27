e7ddfce680c8b3cdab1cf2f432bd0472b715e228
hemant gavali <digitaluser197@gmail.com>
2026-01-28 02:23:24 +0530

    make validated route

---
+++ Backend/nodeS1/Server/Router/ProductRoute.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+    getProduct,
+    addProduct,
+    deleteProduct
+} = require("../controller/ProductConttroller");
+
+const { route } = require("../app");
+
+router.get("/", getProduct);
+router.post("/add", addProduct);
+router.delete("/delete", deleteProduct);
+
+module.exports = router;
