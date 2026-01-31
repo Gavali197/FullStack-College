3c46b3dbab5f04ec4fc901ff1300196991171a3f
hemant gavali <digitaluser197@gmail.com>
2026-01-31 23:59:21 +0530

    handle server side load using memo state

---
+++ Backend/Node Run/back1/Controller/UserController.js
@@ -0,0 +1,5 @@
+const user = require("../Model/UserModel");
+
+exports.getuser = async (req, res, next) =>{
+    
+}
---
+++ Backend/Node Run/back1/Model/UserModel.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+    name : {
+        type : String,
+        require : true
+    },
+    age : {
+        type : Number,
+        require : true
+    },
+    city : {
+        type : String,
+        require : true
+    }
+
+})
+
+module.exports = mongoose.model("user", UserSchema);
