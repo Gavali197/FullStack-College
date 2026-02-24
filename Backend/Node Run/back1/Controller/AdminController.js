c5316f6ebe48210fd9f61e3504ce13e1c3ecec49
hemant gavali <digitaluser197@gmail.com>
2026-02-25 00:13:55 +0530

    Admin Controller post Method

--- Backend/Node Run/back1/Controller/AdminController.js
+++ Backend/Node Run/back1/Controller/AdminController.js
@@ -1,9 +1,32 @@
 const admin = require("../Model/AdminModel")
 
-exports.getUser = async(req, res, next) => {
-    try{
-        const 
-    }catch(){
 
+exports.getAdmin = async(req, res, next) => {
+   try {
+          const findUser = await admin.find();
+          if (!findUser) {
+              return res.status(401).json({
+                  message: "Not User avaliable"
+              })
+          }
+          res.json(findUser);
+      } catch (err) {
+          next(err)
+      }
+}
+
+
+
+exports.postAdmin = async (req, res, next) =>  {
+    try{
+    const createAdmin = await admin.create();
+    if(!createAdmin){
+        return res.status(401).json({
+            message : "Some error From Post Side"
+        })
+    }
+    res.json(createAdmin)
+    }catch(err){
+        next(err)
     }
 }
