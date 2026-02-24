8e33deed9228eb345e6ea0551a2c7a3ee5e71639
hemant gavali <digitaluser197@gmail.com>
2026-02-25 00:16:37 +0530

    Admin Router Get, Post Method Call

--- Backend/Node Run/back1/Routes/AdminRoutes.js
+++ Backend/Node Run/back1/Routes/AdminRoutes.js
@@ -1,3 +1,9 @@
 const express = require("express")
 const router = express.Router();
 
+const {getAdmin, postAdmin} = require("../Controller/AdminController")
+
+router.get("/get", getAdmin);
+router.post("/post", postAdmin);
+
+module.exports = router
