38eab1dc2c1169eb51848e1868fc581688e279ee
hemant gavali <digitaluser197@gmail.com>
2026-02-25 12:05:50 +0530

    backend request Display Data

--- Backend/Node Run/front1/src/App.jsx
+++ Backend/Node Run/front1/src/App.jsx
@@ -5,12 +5,14 @@ import Delete from "./Component/Delete";
 import Update from "./Component/Update";
 import View from "./Component/view";
 import { Route, Routes } from "react-router-dom";
+import AdminController from "./Component/AdminController";
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
       <Routes>
+        <Route path="/adminController" element={<AdminController />} />
         <Route path="/insert" element={<Insert />} />
         <Route path="/delete" element={<Delete />} />
         <Route path="/update" element={<Update />} />
--- Backend/Node Run/front1/src/Component/AdminController.jsx
+++ Backend/Node Run/front1/src/Component/AdminController.jsx
@@ -1,66 +1,131 @@
-import React from 'react'
-import { useState } from 'react';
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 
 const AdminController = () => {
-    const [form, setform] = useState({name: "", age: "", city:""})
-    const [error, seterror] = useState("");
-    const [success, setsuccess] = useState("");
+  const [form, setform] = useState({ name: "", age: "", city: "" });
+  const [error, seterror] = useState("");
+  const [success, setsuccess] = useState("");
+  const [data, setdata] = useState([]);
 
-
-    const insertHandle = async (e) => {
-        e.preventDefault();
-        if(!form.name || !form.age || !form.city){
-            seterror("Fill All Filed");
-        }else{
-            seterror("");
-        }
-
-        console.log(form);
-
-        try{
-            seterror("");
-            const res = await fetch("http://localhost:4000/userlist/api/v2/postAdmin",{
-                method:"POST",
-                headers : {
-                    "Content-Type" : "Application/json",
-                },
-                body : JSON.stringify(form),
-            });
-
-            const data = await res.json();
-
-            if(res.ok){
-                setform({name:"", age:"", city:""});
-                setsuccess("Data Inserted");
-            }else{
-                setform(data.message || "Failed Inserted Data");
-            }
-        }catch(err){
-            seterror("Server Not reachable check backend");
-            console.error(err);
-        }
+  /*
+! Insert user Form Inserthandler Function
+*/
+  const insertHandle = async (e) => {
+    e.preventDefault();
+    if (!form.name || !form.age || !form.city) {
+      seterror("Fill All Filed");
+    } else {
+      seterror("");
     }
 
-    const handleChange = (e) => {
-        const {name, value} = e.target;
-        setform({...form, [name]:value})
+    console.log(form);
+
+    try {
+      seterror("");
+      const res = await fetch(
+        "http://localhost:4000/userlist/api/v2/postAdmin",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "Application/json",
+          },
+          body: JSON.stringify(form),
+        },
+      );
+
+      const data = await res.json();
+
+      if (res.ok) {
+        setform({ name: "", age: "", city: "" });
+        setsuccess("Data Inserted");
+      } else {
+        setform(data.message || "Failed Inserted Data");
+      }
+    } catch (err) {
+      seterror("Server Not reachable check backend");
+      console.error(err);
     }
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setform({ ...form, [name]: value });
+  };
+/*
+! Get Data From Server handle Server Request
+*/
+  const getdata = async () => {
+    try {
+      const res = await fetch(
+        "http://localhost:4000/userlist/api/v2/getAdmin",
+      );
+      const result = await res.json();
+      setdata(result);
+    } catch (err) {
+      console.error("Error Fetching Data : ", err);
+    }
+  };
+
+  useEffect(() => {
+    getdata();
+  }, []);
+
   return (
     // <div></div>
-    <form onSubmit={insertHandle}>
-        {success && <p style={{color:"green"}}>{success}</p>}
-        <h1>
-            Insert Admin Of The Side
-        </h1>
+    <>
+      <form onSubmit={insertHandle}>
+        {success && <p style={{ color: "green" }}>{success}</p>}
+        <h1>Insert Admin Of The Side</h1>
+        name{" "}
+        <input
+          type="text"
+          value={form.name}
+          onChange={handleChange}
+          name="name"
+        />
+        Age{" "}
+        <input
+          type="text"
+          value={form.age}
+          onChange={handleChange}
+          name="age"
+        />
+        City{" "}
+        <input
+          type="text"
+          value={form.city}
+          onChange={handleChange}
+          name="city"
+        />
+        <button type="submit">Insert data</button>
+        {error && <p style={{ color: "red" }}>{error}</p>}
+      </form>
 
-        name <input type="text" value={form.name} onChange={handleChange} name='name'/>
-        Age <input type="text" value={form.age} onChange={handleChange} name='age'/>
-        City <input type="text" value={form.city} onChange={handleChange} name='city'/>
+      <div className="userlist">
+        <h1>User List</h1>
+        <table border={1} cellPadding={10}>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Age</th>
+              <th>City</th>
+            </tr>
+          </thead>
 
-        <button type='submit'>Insert data</button>
-        {error && <p style={{color: "red"}}>{error}</p>}
-    </form>
-  )
-}
+          <tbody>
+            {data.map((item, index) => (
+              <tr key={index}>
+                <td>{item.name}</td>
+                <td>{item.age}</td>
+                <td>{item.city}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+};
 
-export default AdminController
+export default AdminController;
