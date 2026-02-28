1057b289ebd28c22f6cf8b8a3f019103d22ed438
hemant gavali <digitaluser197@gmail.com>
2026-02-28 23:34:25 +0530

    fix error from api handler

--- Backend/Node Run/back1/Controller/AdminController.js
+++ Backend/Node Run/back1/Controller/AdminController.js
@@ -1,32 +1,30 @@
 const admin = require("../Model/AdminModel")
 
 
-exports.getAdmin = async(req, res, next) => {
-   try {
-          const findUser = await admin.find();
-          if (!findUser) {
-              return res.status(401).json({
-                  message: "Not User avaliable"
-              })
-          }
-          res.json(findUser);
-      } catch (err) {
-          next(err)
-      }
+exports.getAdmin = async (req, res, next) => {
+    try {
+        const findUser = await admin.find();
+        if (!findUser) {
+            return res.status(401).json({
+                message: "Not User avaliable"
+            })
+        }
+        res.json(findUser);
+    } catch (err) {
+        next(err)
+    }
 }
 
-
-
-exports.postAdmin = async (req, res, next) =>  {
-    try{
-    const createAdmin = await admin.create(req.body);
-    if(!createAdmin){
-        return res.status(401).json({
-            message : "Some error From Post Side"
-        })
-    }
-    res.json(createAdmin)
-    }catch(err){
+exports.postAdmin = async (req, res, next) => {
+    try {
+        const createAdmin = await admin.create(req.body);
+        if (!createAdmin) {
+            return res.status(401).json({
+                message: "Some error From Post Side"
+            })
+        }
+        res.json(createAdmin)
+    } catch (err) {
         next(err)
     }
 }
@@ -34,43 +32,53 @@ exports.postAdmin = async (req, res, next) =>  {
 
 
 exports.deleteAdmin = async (req, res, next) => {
-    try{
+    try {
         await admin.findByIdAndDelete(
             req.params.id,
             req.body,
             {
-                new : true,
-                runValidators : true
+                new: true,
+                runValidators: true
             }
         );
 
-        if(!this.deleteAdmin){
+        if (!this.deleteAdmin) {
             return res.status(401).json({
-                message : "not found"
+                message: "not found"
             })
         }
 
         res.json({
-            message : "User delete successfully",
-            user : this.deleteAdmin
+            message: "User delete successfully",
+            user: this.deleteAdmin
         })
-    }catch(err){
+    } catch (err) {
         next(err + "inssue in update")
     }
 }
 
 exports.UpdateAdmin = async (req, res, next) => {
-    try{
-      const updatedUser =  await admin.findByIdAndUpdate(
-        req.params.id,
-        req.body,{
-            new : true,
-            runValidators : true
+    try {
+        const updatedUser = await admin.findByIdAndUpdate(
+            req.params.id,
+            req.body, {
+            new: true,
+            runValidators: true
         }
-      );
+        );
 
-      
-    }catch(err){
+        if (!updatedUser) {
+            return res.status(401).json({
+                message: "Not Found"
+            })
+        }
+
+        res.json({
+            message: "User Update Successfully",
+            admin: updatedUser
+        })
+
+    } catch (err) {
         next(err + "The Error Of update Controller")
     }
 }
