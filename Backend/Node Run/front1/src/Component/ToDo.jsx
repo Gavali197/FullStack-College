2af075ce0a26e391f2ad9247c9f794df760d7722
hemant gavali <digitaluser197@gmail.com>
2026-03-03 21:24:13 +0530

    ListOut Task

--- Backend/Node Run/front1/src/Component/ToDo.jsx
+++ Backend/Node Run/front1/src/Component/ToDo.jsx
@@ -1,16 +1,17 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const ToDo = () => {
   const [error, seterror] = useState("");
   const [success, setsuccess] = useState("");
   // Changed "status" to "Status" to match your Mongoose Schema
   const [form, setform] = useState({ task: "", date: "", Status: "Pending" });
+  const [data, setdata] = useState([]);
 
   const API = "http://localhost:4000/userlist/api/v2";
 
   const handleChange = (e) => {
     // FIX 1: Use lowercase 'value'
-    const { name, value } = e.target; 
+    const { name, value } = e.target;
     setform({ ...form, [name]: value });
   };
 
@@ -45,12 +46,26 @@ const ToDo = () => {
     }
   };
 
+  const GetData = async () => {
+    try {
+      const res = await fetch(`${API}/findTask`);
+      const result = await res.json();
+      setdata(result);
+    } catch (err) {
+      seterror(err + "Error from Finding Data");
+    }
+  };
+
+  useEffect(() => {
+    GetData();
+  });
+
   return (
     <>
       {success && <p style={{ color: "green" }}>{success}</p>}
       <form onSubmit={handleAdd}>
         <h2>Todo With Database</h2>
-        
+
         <label>Task: </label>
         <input
           type="text"
@@ -82,6 +97,27 @@ const ToDo = () => {
         <button type="submit"> Add Task </button>
       </form>
       {error && <p style={{ color: "red" }}>{error}</p>}
+
+      <div className="tableList">
+        <table border={1} cellPadding="10">
+          <thead>
+            <tr>
+              <th>Task</th>
+              <th>Date</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((item) => (
+              <tr key={item._id}>
+                <td>{item.task}</td>
+                <td>{item.date}</td>
+                <td>{item.Status}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </>
   );
 };
