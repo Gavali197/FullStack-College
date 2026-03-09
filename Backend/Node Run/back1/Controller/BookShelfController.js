00e511e8d9109365d450dbd18959f0e62ca5e0e9
hemant gavali <digitaluser197@gmail.com>
2026-03-10 00:40:15 +0530

    Manage book using controller

--- Backend/Node Run/back1/Controller/BookShelfController.js
+++ Backend/Node Run/back1/Controller/BookShelfController.js
@@ -1,5 +1,31 @@
 const books = require("../Model/BookShelfModel");
 
 exports.postBook = async (req, res, next) => {
-    
+    try{
+        const post = await books.create(req.body);
+        if(!post){
+            return res.status(401).json({
+                message : "Server Error from Book Controller add"
+            })
+        }
+
+        res.json(post)
+    }catch(err){
+        next(err)
+    }
+}
+
+exports.get = async (req, res, next) => {
+ try{
+        const getData = await books.find();
+        if(!getData){
+            return res.status(401).json({
+                message : "Server Error from Book Controller add"
+            })
+        }
+
+        res.json(getData)
+    }catch(err){
+        next(err)
+    }
 }
