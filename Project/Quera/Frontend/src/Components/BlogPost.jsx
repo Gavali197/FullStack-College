16dc2c992c4ee6ea7ce407144757c7c8c08601ab
hemant gavali <digitaluser197@gmail.com>
2026-03-19 00:36:18 +0530

    After successfully post blog store based on id wise

--- Project/Quera/Frontend/src/Components/BlogPost.jsx
+++ Project/Quera/Frontend/src/Components/BlogPost.jsx
@@ -1,56 +1,83 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from "react";
+// import { postBlog } from "../../../backend/Controller/BlogPostController";
 
 const BlogPost = () => {
-  const [form, setform] = useState({heading:"", description:"", author:""});
+  const [form, setform] = useState({
+    heading: "",
+    description: "",
+    author: "",
+  });
   const [error, seterror] = useState("");
   const [data, setdata] = useState([]);
 
-  const API = "localhost:3030/api/v2/"
+  const API = "http://localhost:3030/api/v2";
 
-
-  const handleForm = async(e) => {
+  const handleForm = async (e) => {
     e.preventDefault();
 
-    if(!form.heading || !form.description || !form.author){
+    if (!form.heading || !form.description || !form.author) {
       seterror("field empty ");
     }
 
-    try{
+    try {
       const getData = await fetch(`${API}/postblog`, {
         method: "POST",
-        headers : {"Content-Type" : "Application/json"},
-        body:JSON.stringify(form)
-      }) 
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(form),
+      });
 
-      if(getData.ok){
+      if (getData.ok) {
         const newBlog = await getData.json();
         setdata((prev)=> [...prev, newBlog]);
+        setform({ heading: "", description: "", author: "" });
         console.log("Post Blog Success");
-      }else{
-      const errorData = await getData.json()
-      seterror(errorData.message || "Failed To post blog")
+      } else {
+        const errorData = await getData.json();
+        seterror(errorData.message || "Failed To post blog");
       }
-    }catch(err){
-      seterror(err + "post side server issue")
+      
+    } catch (err) {
+      seterror(err + "post side server issue");
     }
-  }
+  };
 
   const onchange = (e) => {
-    const [name, value ] = e.target;
-    setform({...form, [name]: value});
+    const { name, value } = e.target;
+    setform({ ...form, [name]: value });
   };
 
   return (
     <div>
       <form onSubmit={handleForm}>
-      heading <input type="text" value={form.heading} name='heading' onChange={onchange} placeholder='Big Heading of blog'  />
-      description <textarea name="description" value={form.description} onChange={onchange} rows={4} cols={50} placeholder='write what your things...'></textarea>
-      author <input type="text" value={form.author} name='author' onChange={onchange} />     
-      <button type='submit'>Post Blog</button>
-      {error && <p style={{color:"red", fontWeight:"bold"}}>{error}</p>} 
+        heading{" "}
+        <input
+          type="text"
+          value={form.heading}
+          name="heading"
+          onChange={onchange}
+          placeholder="Big Heading of blog"
+        />
+        description{" "}
+        <textarea
+          name="description"
+          value={form.description}
+          onChange={onchange}
+          rows={4}
+          cols={50}
+          placeholder="write what your things..."
+        ></textarea>
+        author{" "}
+        <input
+          type="text"
+          value={form.author}
+          name="author"
+          onChange={onchange}
+        />
+        <button type="submit">Post Blog</button>
+        {error && <p style={{ color: "red", fontWeight: "bold" }}>{error}</p>}
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default BlogPost
+export default BlogPost;
---
+++ Project/Quera/Frontend/src/Components/Dashboard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div>Posts</div>
+  )
+}
+
+export default Dashboard
