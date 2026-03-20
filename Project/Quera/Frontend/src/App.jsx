7436521bfb5840cef6307242d4267d841208a052
hemant gavali <digitaluser197@gmail.com>
2026-03-20 18:51:07 +0530

    Making two more component handle blogs

--- Project/Quera/Frontend/src/App.jsx
+++ Project/Quera/Frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import BlogPost from "./Components/BlogPost";
+import Dashboard from "./Components/Dashboard";
+import Blogs from "./Components/Blogs";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Dashboard />} />
         <Route path="/blogpost" element={<BlogPost />} />
+        <Route path="/blogs/:id" element={<Blogs />} />
       </Routes>
     </BrowserRouter>
   );
--- Project/Quera/Frontend/src/Components/Dashboard.jsx
+++ Project/Quera/Frontend/src/Components/Dashboard.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import "./style.css"
 
 const Dashboard = () => {
-  return (
-    <div>Posts</div>
-  )
-}
+  const [data, setdata] = useState([]);
+  const API = "http://localhost:3030/api/v2";
 
-export default Dashboard
+  const getData = async () => {
+    try {
+      const res = await fetch(`${API}/getblog`);
+      const result = await res.json();
+      setdata(result);
+    } catch (err) {
+      console.error(err + "error from get side");
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+  return (
+    <>
+    <div className="dashboard">
+      <h1 className="heading">Heading Page Of new Blog Posted Here</h1>
+      <div className="blog-container">
+        {data.map((item, index) => (
+          <div className="blog" key={index}>
+            <h3>{item.heading}</h3>
+            <p>{item.description}</p>
+            <p>
+              <i>
+                <b>{item.author}</b>
+              </i>
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
+
+    </>
+      );
+};
+
+export default Dashboard;
