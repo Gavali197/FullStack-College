b55d83c4a30b0dce2d03580a65928d58875049d0
hemant gavali <digitaluser197@gmail.com>
2026-04-07 01:10:41 +0530

    profile manage

---
+++ Project/Quera/Frontend/src/Components/Profile.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+
+const Profile = () => {
+  const user = [];                                                       
+
+  return (
+    <div>Profile</div>
+  )
+}
+
+export default Profile
